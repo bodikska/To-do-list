785d0a0716950b96d2c72df56a1942e1cd710b1c
Bogdan Znoyko <bogdanznoyko@MacBook-Air-Bogdan.local>
2022-07-18 18:53:05 +0300

    Add localStorage

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
 let addOrder = document.querySelector('.btn_submit');
+let allList = [];
 
-function AddOrderList() {
-    let infOrder = document.querySelector('.newOrder');
+function loadAllList() {
+    let localSt = localStorage.getItem('allList');
+    localSt = JSON.parse(localSt);
+    allList = localSt;
+    localSt.map((item,index) => {
+
     let newBlock = document.createElement('div');
-    newBlock.className = 'orders';
-    let paragraf = document.createElement('p');
-    paragraf.className = 'ordersParagraf';
-    paragraf.innerHTML = infOrder.value;
-    newBlock.appendChild(paragraf);
     let deleteOrder = document.createElement('input');
+    let pointCheck = document.createElement('input');
+    let paragraf = document.createElement('p');
+    let secondBlock = document.createElement('div');
+
+    newBlock.className = 'orders';
+    paragraf.className = 'ordersParagraf';
+    paragraf.innerHTML = item.text;
+
+    
+
+    newBlock.appendChild(paragraf);
     deleteOrder.className = 'basket';
     deleteOrder.type = 'image';
     deleteOrder.src = 'basket.svg';
-    let pointCheck = document.createElement('input');
     pointCheck.type = 'checkbox';
     pointCheck.className = 'Allchecked';
-    let secondBlock = document.createElement('div');
     secondBlock.className = 'chPointDel';
     secondBlock.appendChild(pointCheck);
     secondBlock.appendChild(deleteOrder);
@@ -23,14 +32,70 @@ function AddOrderList() {
     document.querySelector('.all-orders').appendChild(newBlock);
     reordersBasket();
     isChecked();
+    if (item.checked ==true) {
+        let AllParagraf = document.querySelectorAll('.ordersParagraf')
+        AllParagraf[index].style.textDecoration = 'line-through';
+        let allCkeckBox = document.querySelectorAll('.Allchecked')
+        allCkeckBox[index].checked = true;
+    }
+    })
+   
+    
+}
+if (localStorage.getItem('allList')!=null) {
+    loadAllList();
+}
+
+//  setTimeout({
+   
+//  },500);
+function AddOrderList() {
+    let infOrder = document.querySelector('.newOrder');
+    let newBlock = document.createElement('div');
+    let paragraf = document.createElement('p');
+    let deleteOrder = document.createElement('input');
+    let pointCheck = document.createElement('input');
+    let secondBlock = document.createElement('div');
+
+
+    newBlock.className = 'orders';
+    paragraf.className = 'ordersParagraf';
+    paragraf.innerHTML = infOrder.value;
+    newBlock.appendChild(paragraf);
+    deleteOrder.className = 'basket';
+    deleteOrder.type = 'image';
+    deleteOrder.src = 'basket.svg';
+    pointCheck.type = 'checkbox';
+    pointCheck.className = 'Allchecked';
+    secondBlock.className = 'chPointDel';
+    secondBlock.appendChild(pointCheck);
+    secondBlock.appendChild(deleteOrder);
+    newBlock.appendChild(secondBlock);
+    document.querySelector('.all-orders').appendChild(newBlock);
+    reordersBasket();
+    isChecked();
+    let newListItem = {
+        'text':infOrder.value,
+        'checked':false
+    };
+    allList.push(newListItem);
+    console.log(allList);
     infOrder.value = '';
+    localStorage.setItem('allList', JSON.stringify(allList));
 }
 addOrder.addEventListener('click', AddOrderList);
 
 // удаление задания из спика листа
  function deleteOrder() {
      let pointBasket = event.target;
+     const allBasket = document.querySelectorAll('.basket');
+     const indexBasket = [...allBasket];
+     let index = indexBasket.indexOf(pointBasket);
+     console.log(index);
+     allList.splice(index,1);
      pointBasket.closest('.orders').remove();
+     localStorage.setItem('allList', JSON.stringify(allList));
+
  }
   function reordersBasket() {
  let allBasket = document.querySelectorAll('.basket');
@@ -45,9 +110,14 @@ addOrder.addEventListener('click', AddOrderList);
     for (let i = 0; i<= allOrders.length-1; i++) { 
         if(this.closest('.orders')==allOrders[i]) {
             if (this.checked) {
-            AllParagraf[i].style.textDecoration = 'line-through';
+                AllParagraf[i].style.textDecoration = 'line-through';
+                allList[i].checked = true;
+                localStorage.setItem('allList', JSON.stringify(allList));
+
             } else {
                 AllParagraf[i].style.textDecoration = 'none';
+                allList[i].checked = false;
+                localStorage.setItem('allList', JSON.stringify(allList));
             }
         } 
     }
